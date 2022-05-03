05f568531f3e16873f61a817decd6dfab8cf6951
Drew <dcm752052@gmail.com>
2022-05-03 13:12:43 -0400

    Created hamburger menu. Made support page responsive. Removed shadowing from support item images.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
 $(document).ready(function() {
-    $("#player").selectmenu();
+  $("#player").selectmenu();
 
-    $("#reason").selectmenu();
+  $("#reason").selectmenu();
 
-    $("input[type=submit]").button();
+  $("input[type=submit]").button();
 
-    $('form').validation();
+  $('form').validation();
+
+  // Hamburger Menu
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+  });
+
+  document.querySelectorAll(".nav-links li a").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+  }));
     
   });
