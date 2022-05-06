2f323fb5677e4f9373f25e84a7b07b0f2be764d7
Drew <dcm752052@gmail.com>
2022-05-05 22:43:58 -0400

    Implemented 2nd AJAX request to display players from json.

---
+++ js/players.js
@@ -0,0 +1,24 @@
+var counter = 1;
+var container = document.getElementById("content");
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', '/players.json');
+    ourRequest.onload = function() {
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+        
+    };
+    ourRequest.send();
+    counter++;
+});
+
+function renderHTML(data) {
+    var htmlString = "";
+
+    for (i = 0; i < data.length; i++) {
+        htmlString += "<h3>" + data[i].steamID  + "</h3>" + "<h4><a href=\"" + data[i].steamURL + "\">" + data[i].steamURL + "</a>" + "</h4>";
+    }
+
+    container.insertAdjacentHTML('beforeend', htmlString)
+}
--- js/updateServerStats.js
+++ js/updateServerStats.js
@@ -1,4 +1,5 @@
 var container = document.getElementById("num-vip");
+var container1 = document.getElementById("num-players");
 
 // Get number of VIPs from JSON and displays it on home page
 document.addEventListener("DOMContentLoaded", function(event) { 
@@ -10,4 +11,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
         container.innerHTML = ourData.length + " VIPs"
     };
     ourRequest.send();
+
+    var ourRequest1 = new XMLHttpRequest();
+    ourRequest1.open('GET', '/players.json');
+    ourRequest1.onload = function() {
+        var ourData1 = JSON.parse(ourRequest1.responseText);
+        console.log(ourData1.length);
+        container1.innerHTML = ourData1.length + " Players"
+    };
+    ourRequest1.send();
 });
